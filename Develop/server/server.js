bd84244c0536070cf11e5ba2c81496b0d6d6763c
Cucius <Rylan19@gmail.com>
2022-10-26 02:09:40 -0600

    Deploy to Heroku v4

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
 const routes = require('./routes');
-const { ApolloServer } = require('apollo-server-express')
+
+// Bring in Apollo Server/typeDefs/Resolvers
+const { ApolloServer } = require("apollo-server-express");
 const {typeDefs, resolvers} = require("./schemas");
+
+const db = require('./config/connection');
 const {authMiddleware}=require('./utils/auth');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//  Apollo
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -16,23 +21,25 @@ const server = new ApolloServer({
 });
 
 // Apply Apollo as Middleware
-server.applyMiddleware({app});
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+app.get("*", (req,res)=> {
+  res.sendFile(path.join(__dirname,"../client/build/index.html"));
+})
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+db.once("open", () => {
+  app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
