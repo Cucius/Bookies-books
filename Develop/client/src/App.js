03e6445b518d0fdbce296a9f5143b40f62344758
Cucius <Rylan19@gmail.com>
2022-10-25 23:34:35 -0600

    Added resolvers typeDefs index.js/Added Apollo/ Added mutations

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -4,6 +4,37 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
+import {setContext} from "@apollo/client/link/context"
+
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache
+})
+
+const authLink = setContext((_, { headers }) => {
+  
+  const token = localStorage.getItem("id_token");
+  
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 function App() {
   return (
     <Router>
